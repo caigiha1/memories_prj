ada5a629f224cbfb896de45697527f27b9f97ba0
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-08-04 13:27:32 +0700

    commit 2

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL =
   'mongodb+srv://caigiha3:12111997aB@cluster0.jgusngn.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
